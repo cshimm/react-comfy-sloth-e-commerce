16e193c13b9a29679d166b4d5aab842fa4f094a1
Cameron Shimmin <cameronshimmin@Camerons-Mac-mini.local>
2020-11-29 15:33:22 -0800

    layout

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,11 +1,32 @@
-import React from 'react'
-import styled from 'styled-components'
-import { PageHero } from '../components'
-import aboutImg from '../assets/hero-bcg.jpeg'
+import React from "react";
+import styled from "styled-components";
+import { PageHero } from "../components";
+import aboutImg from "../assets/hero-bcg.jpeg";
 
 const AboutPage = () => {
-  return <h4>about page</h4>
-}
+  return (
+    <main>
+      <PageHero title="about" />
+      <Wrapper className="page section section-center">
+        <img src={aboutImg} alt="desk" />
+        <article>
+          <div className="title">
+            <h2>our story</h2>
+            <div className="underline"></div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur
+              quas maxime, provident a dolorem saepe blanditiis ex nesciunt,
+              illum debitis facere quia veniam dolore corrupti eaque doloremque
+              minima? Illo magnam quo assumenda id iste officiis omnis quaerat
+              ipsum dolor aut eaque dolorem earum natus incidunt, fuga iure
+              ratione doloremque sit provident.
+            </p>
+          </div>
+        </article>
+      </Wrapper>
+    </main>
+  );
+};
 
 const Wrapper = styled.section`
   display: grid;
@@ -33,5 +54,5 @@ const Wrapper = styled.section`
   @media (min-width: 992px) {
     grid-template-columns: 1fr 1fr;
   }
-`
-export default AboutPage
+`;
+export default AboutPage;
